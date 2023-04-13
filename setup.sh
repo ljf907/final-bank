30b5bc976ee96d337ae766786783258ed67822ae
ljf907 <127730744+ljf907@users.noreply.github.com>
2023-04-13 14:26:03 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"ljf907\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+" > ~/.streamlit/config.toml
